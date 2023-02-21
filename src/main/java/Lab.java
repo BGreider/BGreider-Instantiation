45fde0a40a9fd104fdfc71e6338f1371bfd1d452
BGreider <bgreider@gmail.com>
2023-02-21 20:24:20 +0000

    Greider_lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,9 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        return "This is not null";
     }
+    Object obj1 = new Object();
+
+    
 }
